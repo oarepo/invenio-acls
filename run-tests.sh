5d982b35ebae032276990379970919a805b8d3d2
Miroslav Bauer <mirekys@gmail.com>
2019-04-02 15:11:00 +0200

    Add Travis runner script

--- run-tests.sh
+++ run-tests.sh
@@ -1,53 +1,31 @@
-#!/usr/bin/env bash
-##
-## Copyright (c) 2019 UCT Prague.
-## 
-## run-tests.sh is part of Invenio Explicit ACLs 
-## (see https://github.com/oarepo/invenio-explicit-acls).
-## 
-## Permission is hereby granted, free of charge, to any person obtaining a copy
-## of this software and associated documentation files (the "Software"), to deal
-## in the Software without restriction, including without limitation the rights
-## to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-## copies of the Software, and to permit persons to whom the Software is
-## furnished to do so, subject to the following conditions:
-## 
-## The above copyright notice and this permission notice shall be included in all
-## copies or substantial portions of the Software.
-## 
-## THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-## IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-## FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-## AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-## LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-## OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-## SOFTWARE.
-##
+#!/usr/bin/env sh
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2015, 2016 CERN.
+#
+# Invenio is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# Invenio is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Invenio; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+# In applying this license, CERN does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
 
-set -e -o pipefail
-
-test -d /tmp/workdir && rm -r /tmp/workdir
-
-rsync -a --exclude __pycache__ /invenio-explicit-acls/ /tmp/workdir
-
-cd /tmp/workdir
-
-pydocstyle invenio_explicit_acls tests docs
-isort -rc -c -df
-check-manifest --ignore ".travis-*,docs/_build*"
-
-pip install -e .
-
-sphinx-build -qnNW docs docs/_build/html
-
-sudo -u es /tmp/elasticsearch/bin/elasticsearch &
-sleep 10
-
-rsync -a --exclude __pycache__ /invenio-explicit-acls/ /tmp/workdir && python setup.py test
-
-#  docker run -it --entrypoint /bin/bash -v $PWD:/invenio-explicit-acls invenio-acls_travis
-# to run locally in docker:
-# reset; rsync -a --delete --exclude __pycache__ /invenio-explicit-acls/ /tmp/workdir && python setup.py build; pytest -s tests/test_no_acl.py
-# to run single test
-# reset; rsync -a --exclude __pycache__ /invenio-explicit-acls/ /tmp/workdir && pytest tests/test_default_acl_unittests.py
-
+pydocstyle invenio_files_rest tests && \
+isort -rc -c -df . && \
+check-manifest --ignore ".travis-*" && \
+sphinx-build -qnNW docs docs/_build/html && \
+python setup.py test && \
+sphinx-build -qnNW -b doctest docs docs/_build/doctest
