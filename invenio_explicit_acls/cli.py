b8f3ea725bf13ebac0a4207c8590e0c508de971c
Mirek Simek <miroslav.simek@gmail.com>
2019-05-08 11:35:08 +0200

    Added ACL reindex to cli (#25)
    * Reindexing ACLs - just for postgresql
    
    * will have a new version

--- invenio_explicit_acls/cli.py
+++ invenio_explicit_acls/cli.py
@@ -26,8 +26,14 @@
 
 import click
 from flask import cli
+from invenio_db import db
+from invenio_indexer.api import RecordIndexer
+from invenio_jsonschemas import current_jsonschemas
+from invenio_records.models import RecordMetadata
 from invenio_search import current_search
+from sqlalchemy import cast
 
+from invenio_explicit_acls.models import ACL
 from invenio_explicit_acls.proxies import current_explicit_acls
 
 
@@ -52,46 +58,49 @@ def prepare(schema):
 @cli.with_appcontext
 def list_schemas():
     """List all schemas registered in invenio."""
-    for schema in current_search.mappings.keys():
+    for schema in current_jsonschemas.list_schemas():
         print("   ", schema)
 
-#
-# TODO: to be refactored
-#
-# @explicit_acls.command()
-# @click.argument('index', required=False)
-# @cli.with_appcontext
-# def list(index):
-#     """
-#         List all acls. If index is set than limit them to the index given
-#     """
-#     if index:
-#         q = ACL.query.filter_by(index=index)
-#     else:
-#         q = ACL.query.all()
-#     for acl in q:
-#         print(acl)
-#         print(textwrap.indent(json.dumps(acl.database_operations, indent=4, ensure_ascii=False), '    '))
-#
-# @explicit_acls.command()
-# @click.option('--acl', default=None)
-# @click.option('--index', default=None)
-# @click.option('--document', default=None)
-# @cli.with_appcontext
-# def reindex(acl, index, document):
-#     if document is not None:
-#         resp = current_explicit_acls.reindex_document(document, acl)
-#     elif acl is not None:
-#         resp = current_explicit_acls.reindex_acl(acl)
-#     elif index is not None:
-#         resp = current_explicit_acls.reindex_index(index)
-#     else:
-#         resp = current_explicit_acls.reindex_all_indices()
-#
-#     print("Return status: ")
-#     for k, v in resp.items():
-#         print(f'    RecordACL "{k[1]}" (id={k[0]}): {v["updated"]} updated documents, '
-#               f'{v["removed"]} documents with ACLs revoked')
-#
-#
-# __all__ = ('explicit_acls', 'setup_model', 'list_doctypes', 'reindex')
+
+@explicit_acls.command(name='full-reindex')
+@click.option('--verbose/--no-verbose', default=False)
+@click.option('--records/--no-records', default=True)
+@cli.with_appcontext
+def full_reindex(verbose, records):
+    """Updates index of all ACLs and optionally reindexes all documents"""
+    # 1. for each ACL update the ACL's index etc
+    if verbose:
+        print('Reindexing ACLs')
+    for acl in ACL.query.all():
+        if verbose:
+            print('Updating ACL representation for', acl)
+        acl.update()
+    if not records:
+        return
+    # 2. for each of ACL enabled indices reindex all documents
+    uuids = set()
+    import sqlalchemy.dialects.postgresql
+    for schema in current_explicit_acls.enabled_schemas:
+        if verbose:
+            print('Getting records for schema', schema)
+        full_schema = current_jsonschemas.path_to_url(schema)
+        # filter all records with the given schema
+        recs = set()
+        recs.update(str(x[0]) for x in db.session.query(RecordMetadata.id).filter(
+            RecordMetadata.json['$schema'] == cast(schema, sqlalchemy.dialects.postgresql.JSONB)
+        ))
+        recs.update(str(x[0]) for x in db.session.query(RecordMetadata.id).filter(
+            RecordMetadata.json['$schema'] == cast(full_schema, sqlalchemy.dialects.postgresql.JSONB)
+        ))
+        if verbose:
+            print('   ... collected %s records' % len(recs))
+        uuids.update(recs)
+
+    if verbose:
+        print('Adding %s records to indexing queue' % len(uuids))
+    RecordIndexer().bulk_index(uuids)
+
+    if verbose:
+        print('Running bulk indexer on %s records' % len(uuids))
+    RecordIndexer(version_type=None).process_bulk_queue(
+        es_bulk_kwargs={'raise_on_error': False})
